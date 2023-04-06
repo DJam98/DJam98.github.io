8315449b0dbdaa06bd22b24785e405c71892df2c
DJam98 <djam98@otd.ink>
2023-04-06 12:01:02 +1000

    Woo Hoo It's Working!

--- a2/e2/arrays-&-for-loops.js
+++ a2/e2/arrays-&-for-loops.js
@@ -8,5 +8,21 @@ var fruitArray = ["banana", "pineapple", "apple", "peach", "orange"];
 
 var mixedArray = [42, "pineapple", true, [3, 6, "nine"], "orange"];
 
-console.log("I'm going to help test the array");
+console.log(fruitArray);
 
+fruitArray[1] = 'tomato';
+
+fruitArray[5] = 'pear';
+
+console.log(fruitArray);
+
+console.log(fruitArray.concat(mixedArray));
+
+for (let fruit of fruitArray){
+        
+    if(fruit === 'tomato'){
+        console.log("but am I a fruit")
+    } else {
+        console.log(fruit)
+    }
+}
